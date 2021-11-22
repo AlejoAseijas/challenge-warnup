ebbd33c68d07775f6e2bb9fce418856335f947ab
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-11-22 19:49:53 -0300

    fix imports

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import Axios from "axios";
+import React from "react";
 
 import { useAppContext } from "../context/appContext";
 import Posts from "./Posts";
